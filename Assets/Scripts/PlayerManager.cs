4ac7c8dceb27009ad4e7a196f6a8f48fc60d9997
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-11-25 14:36:55 +0900

    [update]PlayerManager,ChangeColor,Changemodel,SelectSharpeとAdachiTestシーンの更新

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,124 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    bool ChangePlayer;
+    byte PlayerState;
+
+    ChangeColor color;
+    GameObject s_Color;
+
+    ChangeModel model;
+    GameObject s_Model;
+
+    Vector3 playerPosition;
+    Quaternion playerRotation;
+
+    Renderer rend;
+
+    byte b_color;
+    byte b_sharpe;
+    byte b_tag;
+    //ボタンを押したかどうか
+    bool changed;
+
+    private void Awake()
+    {
+        s_Color = GameObject.Find("PlayerManager");
+        color = s_Color.GetComponent<ChangeColor>();
+
+        s_Model = GameObject.Find("PlayerManager");
+        model = s_Model.GetComponent<ChangeModel>();
+
+        rend = GetComponent<Renderer>();
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangePlayer = false;
+        PlayerState = 0;
+        playerPosition = GameObject.Find("Player").transform.position;
+        playerRotation = GameObject.Find("Player").transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (changed)
+        {
+            Destroy(GameObject.Find("Player"));
+            //生成する(リストからPrefabを読み込む、)
+            GameObject newObj = Instantiate(model.GetModel(b_sharpe), playerPosition, playerRotation);
+            newObj.GetComponent<Renderer>().material = color.GetColor(b_color);
+            newObj.name = ("Player");
+            newObj.tag = Tag();
+            changed = false;
+        }
     }
+
+    string Tag()
+    {
+        string tag;
+
+        switch (b_tag)
+        {
+            case 0:
+                this.tag = "Triangle_Red";
+                break;
+            case 1:
+                this.tag = ("Triangle_Blue");
+                break;
+            case 2:
+                this.tag = ("Triangle_Yellow");
+                break;
+            case 3:
+                this.tag = ("Square_Red");
+                break;
+            case 4:
+                this.tag = ("Square_Blue");
+                break;
+            case 5:
+                this.tag = ("Square_Yellow");
+                break;
+            case 6:
+                this.tag = ("Circle_Red");
+                break;
+            case 7:
+                this.tag = ("Circle_Blue");
+                break;
+            case 8:
+                this.tag = ("Circle_Yellow");
+                break;
+        }
+
+
+        return this.tag;
+    }
+
+
+    #region GetSet関数
+    public byte B_Color
+    {
+        get { return b_color; }
+        set { b_color = value; }
+    }
+
+    public byte B_Sharpe
+    {
+        get { return b_sharpe; }
+        set { b_sharpe = value; }
+    }
+
+    public byte B_Tag
+    {
+        get { return b_tag; }
+        set { b_tag = value; }
+    }
+
+    public bool ModelChandeFlag
+    {
+        set { changed = value; }
+    }
+    #endregion
 }
--- Assets/Scripts/SelectShape.cs
+++ Assets/Scripts/SelectShape.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectShape : MonoBehaviour
 {
+    Button button;
+
+    [SerializeField]
+    private List<Sprite> BTN_sprite = new List<Sprite>();
+
+    SpriteState spriteState = new SpriteState();
+
+    byte spriteNum;
+
+    GameObject s_PlayerManeger;
+    PlayerManager playerManager;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        s_PlayerManeger = GameObject.Find("PlayerManager");
+        playerManager = s_PlayerManeger.GetComponent<PlayerManager>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteNum = 0;
+        spriteState = button.spriteState;
+        SetButtonSprite((byte)Random.Range(0, BTN_sprite.Count));
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            changeButton();
+        }
     }
+
+    //引数で指定された画像をボタンのSpriteに差し込む。
+    public bool SetButtonSprite(byte i)
+    {
+        if (i > BTN_sprite.Count)
+        {
+            Debug.Log("BTN_spriteのリスト外参照が発生しました");
+            return false;
+        }
+        button.GetComponent<Image>().sprite = BTN_sprite[i];
+        spriteNum = i;
+        return true;
+    }
+
+    public void changePlayerModel()
+    {
+        playerManager.B_Color= (byte)(spriteNum % 3);
+        playerManager.B_Sharpe = (byte)(spriteNum / 3);
+        playerManager.B_Tag = spriteNum;
+        playerManager.ModelChandeFlag = true;
+        Debug.Log("PlayerのPrefabを" + (spriteNum % 3) + "カラーを" + ((byte)(spriteNum / 3)) + "に変更しました");
+    }
+
+
+    public void changeButton()
+    {
+        SetButtonSprite((byte)Random.Range(0, BTN_sprite.Count));
+    }
+    public void changeButton(byte i)
+    {
+        SetButtonSprite(i);
+    }
+
+
+
 }
