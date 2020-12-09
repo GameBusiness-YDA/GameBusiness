5486b31b29275883499f15a1028591eab1873f44
phylos096 <43868600+phylos096@users.noreply.github.com>
2020-12-09 13:15:19 +0900

    [add]スクリプト色々。当たり判定。コンボ。

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    /// <summary>
+    /// 0:判定無し 1:クリア 2:ミス
+    /// </summary>
+    byte collisionResult;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.gameObject.tag == other.tag)
+        {
+            collisionResult = 1;
+        }
+        else
+        {
+            collisionResult = 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collisionResult = 0;
+    }
+
+
+    #region GetSet関数
+
+
+    /// <summary>
+    /// 0:判定無し 1:クリア 2:ミス
+    /// </summary>
+    public byte CollisionResult
+    {
+        get { return collisionResult; }
+        set { collisionResult = value; }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/StageData.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create StageData")]
+public class StageData : ScriptableObject
+{
+    public int stageNum;
+    public int walls;
+    public float firstWallSpeed;
+    public float secondWallSpeed;
+    public float thirdWallSpeed;
+}
