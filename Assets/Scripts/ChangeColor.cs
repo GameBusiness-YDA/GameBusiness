b25b9e55e7bb5077085d2a95b60a03f1222cdb11
gai99721 <kgai.yda@gmail.com>
2020-12-09 15:33:34 +0900

    [add & update] WallGeneratorManagerのプレハブ追加と壁のタグ付のためのソース更新

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -8,33 +8,93 @@ public class ChangeColor : MonoBehaviour
     [SerializeField]
     List<Material> setMaterials = new List<Material>();
 
+    [SerializeField]
+    GameObject wg;
+
     Renderer rend;
 
+    public byte i
+    {
+        get;
+        private set;
+    }
+
     //テスト用の変数
-    byte i;
+    public byte j
+    {
+        get;
+        private set;
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
+
+        i = FindObjectOfType<WallGeneration>().ReturnColorNumber();
+
         //生成時に、起動
-        i = (byte)Random.Range(0,setMaterials.Count);
+        j = (byte)Random.Range(0,setMaterials.Count);
+
+
+        if (i == 0)
+        {
+            if (j == 0)
+            {
+                this.gameObject.tag = "Circle_Blue";
+            }
+            else if (j == 1)
+            {
+                this.gameObject.tag = "Circle_Red";
+            }
+            else if (j == 2)
+            {
+                this.gameObject.tag = "Circle_Yellow";
+            }
+        }
+        else if (i == 1)
+        {
+            if (j == 0)
+            {
+                this.gameObject.tag = "Square_Blue";
+            }
+            else if (j == 1)
+            {
+                this.gameObject.tag = "Square_Yellow";
+            }
+            else if (j == 2)
+            {
+                this.gameObject.tag = "Square_Red";
+            }
+        }
+        else if (i == 2)
+        {
+            if (j == 0)
+            {
+                this.gameObject.tag = "Tryangl_Blue";
+            }
+            else if (j == 1)
+            {
+                this.gameObject.tag = "Tryangle_Red";
+            }
+            else if (j == 2)
+            {
+                this.gameObject.tag = "Tryangle_Yellow";
+            }
+        }
 
         rend = GetComponent<Renderer>();
-        
-        changeColors(i);
+
+        changeColors(j);
+
     }
 
     /// <summary>
     /// setMaterialsにセットされたマテリアルに変更をする　
     /// </summary>
     /// <param name="i"></param>
-    public void changeColors(byte i)
+    public void changeColors(byte j)
     {
-        rend.material = setMaterials[i];
+        rend.material = setMaterials[j];
     }
 
-    public Material GetColor(byte i)
-    {
-        return setMaterials[i];
-    }
 }
--- Assets/Scripts/WallGeneration.cs
+++ Assets/Scripts/WallGeneration.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class WallGeneration : MonoBehaviour
 {
+    //追記　List型に変更
     // 生成する壁オブジェクト
     [SerializeField]
-    public GameObject wall;
+    public List<GameObject> wall = new List<GameObject>();
+
+    /*[SerializeField]
+    public GameObject wall;*/
 
     // 生成までの時間
     float generatTime;
 
+    public byte i
+    {
+        get;
+        private set;
+    }
+
+    public byte j
+    {
+        get;
+        private set;
+    }
+
+    public byte saveI
+    {
+        get;
+        private set;
+    }
+
+
+    //追記 タイマーにセットする時間
+    [SerializeField] float defaultgeneratTime;
+
     private void Awake()
     {
-        generatTime = 2.0f;
+        generatTime = defaultgeneratTime;
     }
 
     private void Start()
@@ -24,12 +50,35 @@ public class WallGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         generatTime -= Time.deltaTime;
 
         if (generatTime < 0.0f)
         {
-            Instantiate(wall, new Vector3(0.0f, 1.0f, 10.0f), Quaternion.identity);
-            generatTime = 2.0f;
+
+            i = (byte)Random.Range(0, wall.Count);
+            SaveNumber(i);
+            //j = 1;
+
+            //Instantiate(wall, new Vector3(0.0f, 1.0f, 10.0f), Quaternion.identity);
+            Instantiate(wall[i], new Vector3(8.5f, -3.6f, 10.0f), Quaternion.identity);
+            generatTime = defaultgeneratTime;
+            //generatTime = 2.0f;
         }
     }
+
+    byte SaveNumber(byte num)
+    {
+        saveI = num;
+
+        return saveI;
+    }
+
+    public byte ReturnColorNumber()
+    {
+
+        return saveI;
+
+    }
+
 }
