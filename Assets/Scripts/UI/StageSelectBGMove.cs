dfe20051b889e3d49dda83f3b73d0baab0a30eac
CSato2000 <bambeaman334@gmail.com>
2021-01-20 15:23:16 +0900

    [Update]ステージセレクトの背景をスクロールするように変更

---
+++ Assets/Scripts/UI/StageSelectBGMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageSelectBGMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 100.0f;
+
+    void Update()
+    {
+        transform.localPosition -= new Vector3(Time.deltaTime * speed,0f,0f );
+        if (transform.localPosition.x < -307.0f)
+        {
+            transform.localPosition = new Vector3(307.0f,0f, 0f);
+        }
+    }
+}
